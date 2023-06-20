1579f868541256e8c0b7107e5dcde4928dcb4084
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-06-20 11:18:06 +0200

    ::white_check_mark: validation signup

--- Screens/Signup.jsx
+++ Screens/Signup.jsx
@@ -5,6 +5,9 @@ import axios from 'axios'
 import { defaultStyle, inputStyling } from '../styles/styles'
 import { registerUser } from '../reducers/authSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import {Toast} from 'react-native-toast-message/lib/src/Toast';
+
+import { validateLastName, validateFirstName, validateEmail, validatePassword} from '../validation/validationInput'
 
 const inputOptions = {
     style:inputStyling,
@@ -21,6 +24,8 @@ const Signup = ({navigation}) => {
   const [firstname, setFirstName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const [error, setError] = useState({lastname: '', firstname: '', email: '', password: ''});
 
   const  submitHandler = () => {
 
@@ -40,15 +45,29 @@ const Signup = ({navigation}) => {
       
      
       const userId = response.data.id
-       const user = { userId:userId ,firstname, lastname, email, password}; // Récupérez les données d'inscription du formulaire
+      const user = { userId:userId ,firstname, lastname, email, password}; // Récupérez les données d'inscription du formulaire
        
-        console.log('user avec id', user)
-       dispatch(registerUser(user)); // Dispatchez l'action pour mettre à jour l'utilisateur dans le store
+      console.log('user avec id', user)
+      dispatch(registerUser(user)); // Dispatchez l'action pour mettre à jour l'utilisateur dans le store
       //  console.log('user dans signup', user)
+      
        navigation.navigate('stores')
+       return Toast.show({
+        type: 'success',
+        text1: `Inscription validée`,
+        text2: `Bienvenue ${user.firstname} ${user.lastname} ` 
+      });
     })
     .catch(function (error) {
     console.log('erreur signup',error);
+    if (error.response && error.response.status === 400) {
+      console.log(error.response.data.error);
+      Toast.show({
+        type: 'error',
+        text1: `Erreur d'inscription`,
+        text2: error.response.data.error[0].message 
+      });
+    }
     // console.log(clientData)
     });
    
@@ -66,32 +85,56 @@ const Signup = ({navigation}) => {
         placeholder='Nom'
         keyboardType='default'
         value={lastname}
-        onChangeText={setLastName}
+        // onChangeText={setLastName}
+        onChangeText={(value) => {
+          setLastName(value);
+          setError({...error, lastname: validateLastName(value)});
+        }}
+        style={error.lastname ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
       />
+      {error.lastname ? <Text style={{color: 'red', textAlign:'center'}}>{error.lastname}</Text> : null}
 
       <TextInput 
         {...inputOptions}
         placeholder='Prénom'
         keyboardType='default'
         value={firstname}
-        onChangeText={setFirstName}
+        // onChangeText={setFirstName}
+        onChangeText={(value) => {
+          setFirstName(value);
+          setError({...error, firstname: validateFirstName(value)});
+        }}
+        style={error.firstname ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
       />
+      {error.firstname ? <Text style={{color: 'red', textAlign:'center'}}>{error.firstname}</Text> : null}
 
       <TextInput 
         {...inputOptions}
         placeholder='Email'
         keyboardType='email-address'
         value={email}
-        onChangeText={setEmail}
+        // onChangeText={setEmail}
+        onChangeText={(value) => {
+          setEmail(value);
+          setError({...error, email: validateEmail(value)});
+        }}
+        style={error.email ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
       />
+       {error.email ? <Text style={{color: 'red', textAlign:'center'}}>{error.email}</Text> : null}
     
       <TextInput 
         {...inputOptions}
         placeholder='Mot de passe'
         secureTextEntry={true}
         value={password}
-        onChangeText={setPassword}
+        // onChangeText={setPassword}
+        onChangeText={(value) => {
+          setPassword(value);
+          setError({...error, password: validatePassword(value)});
+        }}
+        style={error.password ? {...inputOptions.style, borderColor: 'red'} : inputOptions.style}
       />
+      {error.password ? <Text style={{color: 'red', textAlign:'center'}}>{error.password}</Text> : null}
 
       <Button
                 style={style.btn} 
